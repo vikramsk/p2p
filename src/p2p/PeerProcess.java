f92bd0aefde0659cead4503bc483be7dce97a104
rvakash <rvakash@gmail.com>
2018-10-17 23:53:01 -0400

    Updated PeerInfo Parser

--- src/p2p/PeerProcess.java
+++ src/p2p/PeerProcess.java
@@ -8,14 +8,14 @@ import java.io.FileReader;
 public class PeerProcess {
     String peerId;
     ArrayList<PeerInfo> connPeers = new ArrayList<PeerInfo>();
-
-	public static void main(String[] args) {
+    
+    public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("The number of arguments passed to the program is " + args.length + " while it should be 1.\nUsage: java PeerProcess <peerId>");
             return;
         }
-		// TODO Auto-generated method stub
-		System.out.println("Launching Peer " + args[0]);
+        // TODO Auto-generated method stub
+        System.out.println("Launching Peer " + args[0]);
         try{
             PeerProcess peerProcess = new PeerProcess();
             peerProcess.run(args);
@@ -27,11 +27,15 @@ public class PeerProcess {
     public void run(String[] args) throws IOException{
         peerId = args[0];
         String eachPeerLine;
-    	BufferedReader in = new BufferedReader(new FileReader("p2p/PeerInfo.cfg"));
+        // Fill the connPeers list with the peers its supposed to establish a connection with instantiate the Peer
+        BufferedReader in = new BufferedReader(new FileReader("p2p/PeerInfo.cfg"));
         while ((eachPeerLine = in.readLine()) != null) {
             String[] tokens = eachPeerLine.split("\\s+");
+            if(tokens[0].equals(peerId)) break;
             connPeers.add(new PeerInfo(tokens[0], tokens[1], tokens[2]));
         }
+        Peer peer = new Peer(peerId, connPeers);
+
     }
 
 }
