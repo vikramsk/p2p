7329430eb3a060f5f3b8cea3b0dc0287217234e9
Vikram <vikram1791@gmail.com>
2018-11-10 16:38:56 -0500

    config: add peer info parser

--- src/p2p/Configs.java
+++ src/p2p/Configs.java
@@ -3,14 +3,20 @@ package p2p;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 // Configs loads the configuration from the 
 // files when the process is launched.
 public final class Configs {
 	
+	static class Constants {
+		static final String CommonConfigPath = "p2p/Common.cfg";
+		static final String PeerInfoConfigPath = "p2p/PeerInfo.cfg";
+	}
+
 	// Common defines the Common config object.
 	public final static class Common {
-		
+
 		// Constants consists of the supported configuration strings
 		// for the Common configurations.
 		static class Constants {
@@ -21,14 +27,14 @@ public final class Configs {
 			static final String FileSize = "FileSize";
 			static final String PieceSize = "PieceSize";
 		}
-		
+
 		static int NumberOfPreferredNeighbors;
 		static int UnchokingInterval;
 		static int OptimisticUnchokingInterval;
 		static String FileName;
 		static int FileSize;
 		static int PieceSize;
-		
+
 		Common(String fileName) {
 			FileReader fr;
 			BufferedReader br;
@@ -68,4 +74,32 @@ public final class Configs {
 			}
 		}
 	}
+
+	public static PeerInfo parsePeerInfos(List<PeerInfo> peerInfos, String fileName, String targetPeer) {
+		FileReader fr;
+		BufferedReader br;
+		PeerInfo peer = null;
+		try {
+			fr = new FileReader(fileName);
+			br = new BufferedReader(fr);
+			String line = br.readLine();
+			while (line != null) {
+				String[] tokens = line.split("\\s+");
+				PeerInfo parsedPeer =  new PeerInfo(tokens[0], tokens[1], Integer.parseInt(tokens[2]));
+				if(tokens[0].equals(targetPeer)){
+					peer = parsedPeer;
+					break;
+				}
+				peerInfos.add(parsedPeer);
+				line = br.readLine();
+			}
+			br.close();
+			fr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return peer;
+	}
 }
+
--- src/p2p/PeerProcess.java
+++ src/p2p/PeerProcess.java
@@ -1,53 +1,32 @@
 package p2p;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.FileReader;
 
 public class PeerProcess {
-    String peerId, address, serverPort;
-    ArrayList<PeerInfo> connToPeers = new ArrayList<PeerInfo>();
-
+    
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("The number of arguments passed to the program is " + args.length + " while it should be 1.\nUsage: java PeerProcess <peerId>");
+            System.out.println("The number of arguments passed to the program is " + args.length 
+            		+ " while it should be 1.\nUsage: java PeerProcess <peerId>");
             return;
         }
-        // TODO Auto-generated method stub
+  
         System.out.println("Launching Peer " + args[0]);
         try{
-            PeerProcess peerProcess = new PeerProcess();
-            peerProcess.run(args);
+           run(args[0]);
         } catch(IOException e){
 			e.printStackTrace();
         }
 	}
 
-    // Run implements peerInfo parser of the file p2p/PeerInfo.cfg
-    // and instantiates a Peer and establishes TCP connection
-    public void run(String[] args) throws IOException{
-        peerId = args[0];
-        peerInfoParser();
-        Peer peer = new Peer(peerId, connToPeers, address, serverPort);
-        peer.establishTCPConnection();
-        // peer.handShake();
-    }
+    public static void run(String peerID) throws IOException{
+    		ArrayList<PeerInfo> neighborPeers = new ArrayList<PeerInfo>();
+        PeerInfo peerInfo = Configs.parsePeerInfos(neighborPeers, Configs.Constants.PeerInfoConfigPath, peerID);
 
-    // peerInfoParser parses p2p/PeerInfo.cfg and fills the connToPeers list 
-    // with the peers before it
-    void peerInfoParser() throws IOException{
-        String eachPeerLine;
-        BufferedReader in = new BufferedReader(new FileReader("p2p/PeerInfo.cfg"));
-        while ((eachPeerLine = in.readLine()) != null) {
-            String[] tokens = eachPeerLine.split("\\s+");
-            if(tokens[0].equals(peerId)){
-                address = tokens[1];
-                serverPort = tokens[2];
-                break;
-            }
-            connToPeers.add(new PeerInfo(tokens[0], tokens[1], tokens[2]));
+        if (peerInfo == null) {
+        		throw new IOException("could not parse peer info");
         }
+        Peer localPeer = new Peer(peerInfo);
+        localPeer.start(neighborPeers);
     }
-
 }
