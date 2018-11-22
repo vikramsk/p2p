4e595c30b4f86ba8dc4620f6a9340b7e65f74fd9
Vikram <vikram1791@gmail.com>
2018-11-22 17:19:27 -0500

    main: optional filepath override

--- src/p2p/PeerProcess.java
+++ src/p2p/PeerProcess.java
@@ -5,12 +5,19 @@ import java.io.IOException;
 public class PeerProcess {
 
 	public static void main(String[] args) {
-		if (args.length != 1) {
+		if (args.length > 2) {
 			System.out.println("The number of arguments passed to the program is " + args.length 
 					+ " while it should be 1.\nUsage: java PeerProcess <peerId>");
 			return;
 		}
-
+		
+		new Configs.Common(Configs.Constants.CommonConfigPath);
+		
+		// option to override the default filename.
+		if (args.length == 2) {
+			Configs.Common.FileName = args[1];
+		}
+		
 		System.out.println("Launching Peer " + args[0]);
 		try{
 			run(args[0]);
@@ -22,7 +29,6 @@ public class PeerProcess {
 	public static void run(String peerID) throws IOException{
 		ArrayList<PeerInfo> neighborPeers = new ArrayList<PeerInfo>();
 		PeerInfo peerInfo = Configs.parsePeerInfos(neighborPeers, Configs.Constants.PeerInfoConfigPath, peerID);
-
 		if (peerInfo == null) {
 			throw new IOException("could not parse peer info");
 		}
